2a10ff817a14f5648ed06f519dab0cfb28d5ab8e
kids01 <hfuewqfo@gmail.com>
2025-03-13 19:02:39 +0300

    Готово

---
+++ main.py
@@ -0,0 +1,64 @@
+from data import db_session
+from data.users import User
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.sqlite")
+
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Случайный"
+    user.name = "Рэнди"
+    user.age = 21
+    user.position = "рассказчик"
+    user.speciality = "рандомайзер"
+    user.address = "Модуль_1"
+    user.email = "rendi@mars.rimworld"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Мамут"
+    user.name = "Рахал"
+    user.age = 43
+    user.position = "Механик"
+    user.speciality = "Автомеханик"
+    user.address = "Модуль_2"
+    user.email = "rahalmamyt@mars.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Morgan"
+    user.name = "Dexter"
+    user.age = 21
+    user.position = "Полицейский"
+    user.speciality = "Специалист по анализу брызг крови"
+    user.address = "modul_Maimi"
+    user.email = "dexer149@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
