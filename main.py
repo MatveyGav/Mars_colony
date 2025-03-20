754d8941614df1749f0bbeda7004795c65d254a7
kids01 <ufe@gmail.com>
2025-03-20 19:47:15 +0300

    Fixed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from data import db_session
 from data.users import User
 from flask import Flask
-import sqlite3
 from data.jobs import Jobs
 import datetime
 
@@ -11,75 +10,41 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.sqlite")
+    db_sess = db_session.create_session()
 
-    curs = sqlite3.connect("db/blogs.sqlite")
-    info = curs.execute('SELECT * FROM users WHERE surname=?', ("Scott",)).fetchone()
-    if len(info) == 0:
-        user = User()
-        user.surname = "Scott"
-        user.name = "Ridley"
-        user.age = 21
-        user.position = "captain"
-        user.speciality = "research engineer"
-        user.address = "module_1"
-        user.email = "scott_chief@mars.org"
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-    info = curs.execute('SELECT * FROM users WHERE surname=?', ("Случайный",)).fetchone()
-    if len(info) == 0:
-        user = User()
-        user.surname = "Случайный"
-        user.name = "Рэнди"
-        user.age = 21
-        user.position = "рассказчик"
-        user.speciality = "рандомайзер"
-        user.address = "Модуль_1"
-        user.email = "rendi@mars.rimworld"
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-    info = curs.execute('SELECT * FROM users WHERE surname=?', ("Мамут",)).fetchone()
-    if len(info) == 0:
-        user = User()
-        user.surname = "Мамут"
-        user.name = "Рахал"
-        user.age = 43
-        user.position = "Механик"
-        user.speciality = "Автомеханик"
-        user.address = "Модуль_2"
-        user.email = "rahalmamyt@mars.ru"
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-    info = curs.execute('SELECT * FROM users WHERE surname=?', ("Morgan",)).fetchone()
-    if len(info) == 0:
-        user = User()
-        user.surname = "Morgan"
-        user.name = "Dexter"
-        user.age = 21
-        user.position = "Полицейский"
-        user.speciality = "Специалист по анализу брызг крови"
-        user.address = "modul_Maimi"
-        user.email = "dexer149@mars.org"
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-    info = curs.execute('SELECT * FROM jobs WHERE team_leader=?', (1,)).fetchone()
-    if info is None:
-        job = Jobs()
-        job.team_leader = 1
-        job.job = "deployment of residential modules 1 and 2"
-        job.work_size = 15
-        job.collaborators = "2, 3"
-        job.start_date = datetime.datetime.now()
-        job.is_finished = False
-        db_sess = db_session.create_session()
+    users_data = [
+        {"surname": "Scott", "name": "Ridley", "age": 21, "position": "captain",
+         "speciality": "research engineer", "address": "module_1", "email": "scott_chief@mars.org"},
+        {"surname": "Случайный", "name": "Рэнди", "age": 21, "position": "рассказчик",
+         "speciality": "рандомайзер", "address": "Модуль_1", "email": "rendi@mars.rimworld"},
+        {"surname": "Мамут", "name": "Рахал", "age": 43, "position": "Механик",
+         "speciality": "Автомеханик", "address": "Модуль_2", "email": "rahalmamyt@mars.ru"},
+        {"surname": "Morgan", "name": "Dexter", "age": 21, "position": "Полицейский",
+         "speciality": "Специалист по анализу брызг крови", "address": "modul_Maimi", "email": "dexer149@mars.org"}
+    ]
+
+    for user_data in users_data:
+        if not db_sess.query(User).filter(User.email == user_data["email"]).first():
+            user = User(**user_data)
+            db_sess.add(user)
+            db_sess.commit()
+
+    job_data = {
+        "team_leader": 1,
+        "job": "deployment of residential modules 1 and 2",
+        "work_size": 15,
+        "collaborators": "2, 3",
+        "start_date": datetime.datetime.now(),
+        "is_finished": False
+    }
+
+    if not db_sess.query(Jobs).filter(Jobs.job == job_data["job"]).first():
+        job = Jobs(**job_data)
         db_sess.add(job)
         db_sess.commit()
+
     app.run()
 
 
 if __name__ == '__main__':
     main()
-
